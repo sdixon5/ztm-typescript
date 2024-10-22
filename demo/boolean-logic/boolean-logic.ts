a451468187171581a2f9e13b8126c6e2f4d0cf2e
sdixon5 <shawn12dixon@gmail.com>
2024-10-22 13:28:53 -0600

    boolean and compound logic

--- demo/boolean-logic/boolean-logic.ts
+++ demo/boolean-logic/boolean-logic.ts
@@ -7,3 +7,27 @@ import { strict as assert } from "assert";
 // express conditions and make decisions based on the truth or falsehood of
 // these conditions.
 //
+
+const yes = true;
+const no = false;
+
+const age = 18;
+const canPurchase = age >= 18;
+assert.equal(canPurchase, true);
+
+const denyPurchase = age < 18;
+assert.equal(denyPurchase, false);
+
+const two = 2;
+const isThree = 1 + two === 3;
+assert.equal(isThree, true);
+
+const isNotThree = 1 + two !== 10;
+assert.equal(isNotThree, true);
+
+//<
+//<=
+//>
+//>=
+//===
+//!==
--- demo/compound-bool/compound-bool.ts
+++ demo/compound-bool/compound-bool.ts
@@ -1,4 +1,30 @@
 /* eslint-disable */
 import { strict as assert } from "assert";
 
+// NOT
+const writing = true;
+const reading = !writing;
 
+assert.equal(reading, false);
+
+// OR
+// (pick 1 movie)
+const rating = 9;
+const favoriteMovie = false;
+
+const suggestMovie = rating > 8 || favoriteMovie;
+assert.equal(suggestMovie, true);
+
+// AND
+const age = 18;
+const isTeenager = age >= 13 && age < 20;
+assert.equal(isTeenager, true);
+
+// our package
+const packageWeight = 30;
+const packageLength = 50;
+const feeExemption = false;
+
+//these compound statements should be broken up into "units" to reduce complexity and chances for mistakes.
+const extraFee = !feeExemption && (packageWeight > 25 || packageLength > 40);
+assert.equal(extraFee, true);
