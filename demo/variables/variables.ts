394b00fbc521ff330511230eb4162b0735eefb16
sdixon5 <shawn12dixon@gmail.com>
2024-10-22 13:06:56 -0600

    Update variables.ts
    end of variables lesson.
    
    surprisingly learned some new stuff.
    
    error lens in VS Code is awesome!

--- demo/variables/variables.ts
+++ demo/variables/variables.ts
@@ -9,3 +9,56 @@
 // Useful links:
 // https://www.typescriptlang.org/docs/handbook/variable-declarations.html#let-declarations
 //
+
+const courseName = "typescript";
+//const courseName2 = 'typescript'; automatic saving is converting to "" instead of ''.
+const courseName2 = "typescript";
+
+const courseName3 = `typescript`;
+
+const amount = 10;
+const fraction = 10.5;
+const oneThousand = 1e3;
+const allPermissions = 0o777;
+const hexByte = 0xff;
+const binary = 0b0100001;
+const bigInt = 9000n;
+
+const yes = true;
+const no = false;
+
+//completely missing
+const missing = undefined;
+
+//we know it's missing
+const empty = null;
+
+let someNum = 0;
+someNum = 1;
+someNum = 2;
+
+//let someNum = 5
+//variable shadowing
+//can be useful in test code
+{
+  let someNum = 5;
+  // test code ...
+}
+{
+  let someNum = 4;
+  // test code ...
+}
+{
+  let someNum = 3;
+  // test code ...
+}
+
+//un-initialized
+let hello;
+
+hello = "hi";
+hello = "hey";
+hello = "hello";
+
+//const declarations must be initialized.
+//const whoops; --> error
