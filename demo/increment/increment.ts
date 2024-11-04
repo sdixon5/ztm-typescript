4a4addd88ad8bddd1f52435631f866f3e37bc236
sdixon5 <shawn12dixon@gmail.com>
2024-11-04 15:57:02 -0700

    Two more lessons completed

--- demo/increment/increment.ts
+++ demo/increment/increment.ts
@@ -8,3 +8,38 @@ import { strict as assert } from "assert";
 // Useful links:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
 //
+
+let n = 1;
+// "postfix" increment
+n++;
+assert.equal(n, 2);
+// "prefix" increment
+++n;
+assert.equal(n, 3);
+
+n = 5;
+const k = n++;
+//assert.equal(k, 6); // --> leads to error. because assignment of k happens first, then ++ happens but only on n variable
+assert.equal(n, 6);
+
+n = 5;
+const j = ++n;
+assert.equal(j, 6); // --> works because ++ happens first, stores in n, then n is stored in j
+
+n = 5;
+const t = --n;
+assert.equal(t, 4);
+
+n = 5;
+n += 5; // --> arithmetic assignment --> works best
+assert.equal(n, 10);
+n -= 5;
+assert.equal(n, 5);
+n -= 1;
+assert.equal(n, 4);
+
+// +=
+// -=
+// *=
+// /=
+// %=
